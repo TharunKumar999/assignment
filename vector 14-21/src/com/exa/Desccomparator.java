501de220afe9b72bf95d01c743035306429319d4
001Z8B744 <001Z8B744@LAPTOP-JG8PKQQ8>
2021-06-14 16:49:42 +0530

    added id

---
+++ vector 14-21/src/com/exa/Desccomparator.java
@@ -0,0 +1,12 @@
+package com.exa;
+	import java.util.Comparator;
+
+	public class Desccomparator implements Comparator{
+
+		@Override
+		public int compare(Object o1, Object o2) {
+			
+			return ((Todo)o1).getDesc().compareTo(((Todo)o2).getDesc());
+		}
+
+}
