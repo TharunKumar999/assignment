22881eb3db59ea41983357c8cf9f9278fcfd548a
001Z8B744 <001Z8B744@LAPTOP-JG8PKQQ8>
2021-06-17 10:07:50 +0530

    commit

--- Thread17-06-21/src/com/ex/app/App.java
+++ Thread17-06-21/src/com/ex/app/App.java
@@ -1,23 +1,26 @@
 package com.ex.app;
 import com.ex.ThreadTest;
 
+import com.ex.PrintThread;
+import com.ex.ThreadTest;
+
 public class App {
 
 	public static void main(String[] args) {
-		ThreadTest test=new ThreadTest();
-
-		Thread t=new Thread(test);
-		System.out.println(t.getName());
-		t.start();
-		Thread t1=new Thread(test);
-		System.out.println(t1.getName());
-		t1.start();
-		Thread t2=new Thread(test);
-		System.out.println(t2.getName());
-		t2.start();
-		Thread  t3=new Thread(test);
-		System.out.println(t3.getName());
-		t3.start();
+		/*
+		 * ThreadTest test=new ThreadTest();
+		 * 
+		 * Thread t=new Thread(test); System.out.println(t.getName()); t.start(); Thread
+		 * t1=new Thread(test); System.out.println(t1.getName()); t1.start(); Thread
+		 * t2=new Thread(test); t2.start();
+		 */
+		
+		PrintThread thread1=new PrintThread("A");
+		thread1.start();
+		PrintThread thread2=new PrintThread("B");
+		thread2.start();
 	}
 
-}
+	}
+
+
