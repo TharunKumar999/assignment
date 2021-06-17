5ab01329876329e5a01f3a3058c29d92a1f01e10
001Z8B744 <001Z8B744@LAPTOP-JG8PKQQ8>
2021-06-17 14:09:30 +0530

    second commit

---
+++ Thread17-06-21/src/comm/race/ThreadTest.java
@@ -0,0 +1,15 @@
+package comm.race;
+
+public class ThreadTest 
+{
+
+	public static void main(String[] args)
+	{
+		TwoString twoString=new TwoString();
+		new PrintStringThread("Hello", "There",twoString);
+		new PrintStringThread("How Are", "You?",twoString);
+		new PrintStringThread("Thank You", "Very Much",twoString);
+
+	}
+}
+
