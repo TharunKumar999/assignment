7aaa5a5dbc67f83e85a6e8005ee994ed453b7c25
001Z8B744 <001Z8B744@LAPTOP-JG8PKQQ8>
2021-06-21 18:14:04 +0530

    hibernate

---
+++ employee/src/main/java/org/example/employee/App.java
@@ -0,0 +1,33 @@
+package org.example.employee;
+
+
+import java.util.Random;
+
+import org.example.demo.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+        SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).buildSessionFactory();
+        Session session=factory.openSession();
+        session.getTransaction().begin();
+        session.save(new Employee(3, "d", "joe", "Djoe@email.com"));
+        //new Random().nextInt()
+        session.getTransaction().commit();
+        System.out.println("one item saved");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+    }
+}
