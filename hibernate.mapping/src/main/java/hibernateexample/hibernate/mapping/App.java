c4c47d7ece7666f22c846c9363bd81ee626fa6c7
001Z8B744 <001Z8B744@LAPTOP-JG8PKQQ8>
2021-06-23 12:38:21 +0530

    commit

--- hibernate.mapping/src/main/java/hibernateexample/hibernate/mapping/App.java
+++ hibernate.mapping/src/main/java/hibernateexample/hibernate/mapping/App.java
@@ -1,4 +1,7 @@
 package hibernateexample.hibernate.mapping;
+
+import java.util.logging.Logger;
+
 import org.example.demo.model.Instructor;
 import org.example.demo.model.InstructorDetails;
 import org.hibernate.Session;
@@ -9,41 +12,55 @@ import org.hibernate.cfg.Configuration;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       try {
-    	   
-    	   SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
-    			   .addAnnotatedClass(InstructorDetails.class).buildSessionFactory();
-    	   Session session=factory.getCurrentSession();
-			
-			  Instructor instructor=new Instructor(); InstructorDetails details=new
-			  InstructorDetails(); details.setHobby("Singing");
-			  details.setYoutubeChannel("channel-1"); instructor.setFirstName("John");
-			  instructor.setLastName("Doe"); instructor.setEmail("john@email.com");
-			  instructor.setInstructorDetails(details); session.getTransaction().begin();
-			  session.persist(instructor); session.getTransaction().commit();
-			 
-    	   //session.getTransaction().begin();
-    	  // Instructor instructor=session.get(Instructor.class, 1);
+
+public class App {
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("org.example.demo.App.class");
+		try {
+
+			SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+					.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetails.class)
+					.buildSessionFactory();
+			Session session = factory.getCurrentSession();
 			/*
+			 * Instructor instructor=new Instructor(); InstructorDetails details=new
+			 * InstructorDetails(); details.setHobby("Singing");
+			 * details.setYoutubeChannel("channel-1"); instructor.setFirstName("John");
+			 * instructor.setLastName("Doe"); instructor.setEmail("john@email.com");
+			 * instructor.setInstructorDetails(details); session.getTransaction().begin();
+			 * session.persist(instructor); session.getTransaction().commit();
+			 * 
+			 * session.getTransaction().begin(); Instructor
+			 * instructor=session.get(Instructor.class, 1);
+			 * 
 			 * InstructorDetails details=instructor.getInstructorDetails();
 			 * details.setHobby("Cricket"); details.setYoutubeChannel("my-channel-1");
 			 * instructor.setEmail("john1@email.com"); session.merge(instructor);
 			 * session.getTransaction().commit();
+			 * 
+			 * 
+			 * session.delete(instructor); session.getTransaction().commit();
+			 * System.out.println("done");
 			 */
-    	 
-    	  // session.delete(instructor);
-    	   //session.getTransaction().commit();
-    	   System.out.println("done");
-    	   
-		
-	} catch (Exception e) {
-		// TODO: handle exception
-		e.printStackTrace();
+			InstructorDetails details = new InstructorDetails();
+			details.setHobby("Singing");
+			details.setYoutubeChannel("channel-1");
+			Instructor instructor = new Instructor();
+			instructor.setFirstName("John");
+			instructor.setLastName("doe");
+			instructor.setEmail("john@email.com");
+			details.setInstructor(instructor);
+			instructor.setInstructorDetails(details);
+			session.getTransaction().begin();
+			session.persist(details);
+			session.persist(instructor);
+			session.getTransaction().commit();
+			logger.info("done");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
 }
-    }
-}
-    
+ 
