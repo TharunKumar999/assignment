3c2bcb12c1a2745e4947d8cdc07903084cd2379a
TharunKumar999 <85543591+TharunKumar999@users.noreply.github.com>
2021-06-28 10:09:54 +0530

    Add files via upload

---
+++ java/org/example/spring_jdbc/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package org.example.spring_jdbc.dao;
+
+import java.util.List;
+
+import org.example.spring_jdbc.model.Employee;
+
+public interface EmployeeDao {
+	
+	public Employee createEmployee(Employee employee);
+	public List<Employee> displayEmployees();
+
+}
